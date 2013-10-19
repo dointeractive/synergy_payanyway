4490b37cf81e4a494e720e098e6ad48de8c74b60
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-10-19 21:36:49 +0600

    Ensure that order exists in result_url

--- app/controllers/spree/gateway/payanyway_controller.rb
+++ app/controllers/spree/gateway/payanyway_controller.rb
@@ -16,7 +16,7 @@ class Spree::Gateway::PayanywayController < Spree::StoreController
   # end
 
   def result
-    if @gateway.result_signature(@order, params) == params['MNT_SIGNATURE'] && complete_or_create_payment(@order, @gateway, params) && complete_order(@order)
+    if @order && @gateway.result_signature(@order, params) == params['MNT_SIGNATURE'] && complete_or_create_payment(@order, @gateway, params) && complete_order(@order)
       render :text => 'SUCCESS'
     else
       render :text => 'FAIL'
